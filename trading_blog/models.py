9441b572b500b7ea0cf72b3fb636d7a991dafd36
Colin Breakell <cbreakell99@gmail.com>
2024-06-03 21:29:43 +0000

    Create Journal Model in trading_blog

--- trading_blog/models.py
+++ trading_blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 # Create your models here.
+class Journal(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="trading_blog_posts"
+    )
+    content = models.TextField()
+    date = models.DateField(auto_now=True, auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    #Image model when I get to cloudinary
