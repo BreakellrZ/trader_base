57505133c3cf3c98555c11285794beec9cc2afd8
Colin Breakell <cbreakell99@gmail.com>
2024-06-04 12:22:52 +0000

    Update Journal model - add in watchlist, news_events, & news optionsfor database

--- trading_blog/models.py
+++ trading_blog/models.py
@@ -3,6 +3,25 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+WATCHLIST = (
+    ('eur/usd','EUR/USD'),
+    ('gbp/usd','GBP/USD'),
+    ('usd/cad','USD/CAD'),
+    ('eur/gbp','EUR/GBP'),
+    ('treasuries','TREASURIES'),
+    ('bond yields', 'BOND YIELDS'),
+    ('s&p500','S&P500'),
+    ('nasdaq100', 'NASDAQ100'),
+    ('dxy','DXY'),
+)
+
+NEWS_EVENTS = (
+    (0, "None"),
+    (1, "High Impact News"),
+    (2, "Medium Impact News"),
+    (3, "Low Impact News"),
+)
+
 
 # Create your models here.
 class Journal(models.Model):
@@ -15,6 +34,14 @@ class Journal(models.Model):
     date = models.DateField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    watchlist = models.CharField(max_length=50, choices=WATCHLIST, default="dxy")
+    news_events = models.IntegerField(choices=NEWS_EVENTS, default=0)
+    news = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f"This post contains {self.symbol}"
+
+
     #Image model when I get to cloudinary
 
 class Comment(models.Model):
