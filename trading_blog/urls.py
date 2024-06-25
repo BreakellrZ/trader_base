5326bbd234cfd7e5b43c3e94b041933a4bdc6aa6
Colin Breakell <cbreakell99@gmail.com>
2024-06-25 22:37:22 +0000

    Create post_detail html file for showing post content - Create Journal post view - Linked Urls for post_detail.html

--- trading_blog/urls.py
+++ trading_blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('tradingblog/', views.PostList.as_view(), name ='trading_blog'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- trading_blog/views.py
+++ trading_blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Journal
 
@@ -8,3 +8,27 @@ class PostList(generic.ListView):
     queryset = Journal.objects.all().order_by("-date")
     template_name = "post.html"
     paginate_by = 3
+
+# Taken code from code institute I think therefore I blog walkthrough
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Journal.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "post_detail.html",
+        {"post": post},
+    )
